564ae70751b12a43f23312458cf0698fb100764d
ika1155 <ishidas1155@gmail.com>
2023-01-18 15:21:27 +0900

    oldヘルパー

--- resources/views/todo_item/create.blade.php
+++ resources/views/todo_item/create.blade.php
@@ -26,8 +26,8 @@
 					transition
 					ease-in-out
 					m-0
-					focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" name="item_name"
-				placeholder="Enter your task">
+					focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" 
+					name="item_name" placeholder="Enter your task" value="{{old('item_name')}}">
 			</div>
 		 	<div class="form-group mb-6">
 				<label for="user_id" class="form-label inline-block mb-2 text-gray-700">担当者</label>
@@ -48,7 +48,7 @@
 					focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" name="user_id">
 					<option hidden>--選択してください--</option>
 					@foreach($users as $user)
-						<option value="{{$user->id}}">{{$user->name}}</option>
+						<option value="{{$user->id}}" @if (old('user_id') == $user->id) selected @endif>{{$user->name}}</option>
 					@endforeach
 				</select>
 		  	</div>
@@ -68,12 +68,13 @@
 					transition
 					ease-in-out
 					m-0
-					focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" name="expire_date">
+					focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" 
+					name="expire_date" value="{{old("expire_date")}}">
 			</div>
 		  	<div class="form-group form-check mb-6">
 				<input type="checkbox"
 			  		class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-			  		name="done">
+			  		name="done" @if (old('done')) checked @endif>
 				<label class="form-check-label inline-block text-gray-800" for="done">完了</label>
 		  	</div>
 			<button type="submit" class="
